c4e2403195b8ff208d3f6c7b271b69d4ad0e6bbf
Anagha Nisakar <anagha@iocod.com>
2022-08-18 17:12:39 +0530

    component

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,51 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1> {{title}}</h1>
+    <input type="text" placeholder="Product" ref="name">
+    <button @click="handleClick">Click Me</button>
+    <div class="product-container">
+        <product v-for="zzzzzzzzzzz in arr" :key="product.id"/>
+    </div>
+
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+// import HelloWorld from './components/HelloWorld.vue'
 
+// import components
+import product from './components/product.vue'
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  components: {product},
+   data() {
+      return {
+        title: 'My First Vue App',
+        arr : [
+          {
+            id: 1,
+            name: 'Shirt',
+            rate: 1200
+          },
+          {
+            id: 2,
+            name: 'T Shirt',
+            rate: 1200
+          },
+          {
+            id: 3,
+            name: 'Skirt',
+            rate: 1200
+          }
+        ]
+      }
+   },
+   methods: {
+    handleClick() {
+      // console.log(this.$refs.name);
+      this.$refs.name.classList.add('input')
+      this.$refs.name.focus()
+    }
+   }
   }
-}
 </script>
 
 <style>
@@ -22,5 +56,18 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+.product-container {
+  width:100%;
+  display:flex;
+  align-items: center;
+  justify-content:center;
+  flex-wrap:wrap;
+  column-gap:20px;
+  row-gap:20px;
+  margin:2vh 0
+}
+.input {
+  background: #ece;
 }
 </style>
---
+++ src/components/product.vue
@@ -0,0 +1,21 @@
+<template>
+  <!-- eslint-disable-next-line vue/max-attributes-per-line -->
+    <div class="product-cont">
+        <div class="product">
+            <p>Product Name</p>
+        </div>
+    </div>
+</template>
+
+<style scoped>
+    .product-cont {
+        width:200px;
+        display:flex;
+    }
+    .product {
+        width:100%;
+        height:200px;
+        background: #fff;
+        border: 1px solid #ccc
+    }
+</style>
